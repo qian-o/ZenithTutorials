d6fcdc536833e46516856a9b24b64f828b62bc30
qian-o <1324771795@qq.com>
2026-01-30 09:36:04 +0800

    Refactor CocoaHelper signatures and CreateLayer logic
    Refactored CocoaHelper by removing the unsafe modifier and correcting the signatures and order of objc_msgSend P/Invoke overloads. Updated CreateLayer to use improved parameter naming and reordered layer setup calls. Note: CAMetalLayer creation and retention code was removed, which may be unintentional.

--- ZenithTutorials/CocoaHelper.cs
+++ ZenithTutorials/CocoaHelper.cs
@@ -1,6 +1,6 @@
 ﻿namespace ZenithTutorials;
 
-internal static unsafe partial class CocoaHelper
+internal static partial class CocoaHelper
 {
     private const string LibObjC = "/usr/lib/libobjc.A.dylib";
 
@@ -14,21 +14,19 @@ internal static unsafe partial class CocoaHelper
     private static partial nint Send(nint receiver, nint selector);
 
     [LibraryImport(LibObjC, EntryPoint = "objc_msgSend")]
-    private static partial nint Send(nint receiver, nint selector, [MarshalAs(UnmanagedType.I1)] bool arg);
-
-    [LibraryImport(LibObjC, EntryPoint = "objc_msgSend")]
     private static partial nint Send(nint receiver, nint selector, nint arg);
 
-    public static nint CreateLayer(nint cocoaWindow)
+    [LibraryImport(LibObjC, EntryPoint = "objc_msgSend")]
+    private static partial nint Send(nint receiver, nint selector, [MarshalAs(UnmanagedType.I1)] bool arg);
+
+    public static nint CreateLayer(nint cocoa)
     {
-        nint nsView = Send(cocoaWindow, Selector("contentView"));
-
-        Send(nsView, Selector("setWantsLayer:"), true);
-
         nint layer = Send(GetClass("CAMetalLayer"), Selector("layer"));
-
         Send(layer, Selector("retain"));
-        Send(nsView, Selector("setLayer:"), layer);
+
+        nint view = Send(cocoa, Selector("contentView"));
+        Send(view, Selector("setLayer:"), layer);
+        Send(view, Selector("setWantsLayer:"), true);
 
         return layer;
     }
